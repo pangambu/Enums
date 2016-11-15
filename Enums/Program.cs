c51823964d84905152e139477b08a9cea040fd43
Paul <pangambu@yahoo.com>
2016-11-14 19:34:01 -0500

    Use of Enums

--- Enums/Program.cs
+++ Enums/Program.cs
@@ -10,6 +10,66 @@ namespace Enums
     {
         static void Main(string[] args)
         {
+
+            Customer[] customers = new Customer[3];
+            customers[0] = new Customer
+            {
+                Name = "Mark",
+                Gender = Gender.Male
+            };
+
+            customers[1] = new Customer
+            {
+                Name = "Mary",
+                Gender = Gender.Female
+            };
+            customers[2] = new Customer
+            {
+                Name = "Sam",
+                Gender = Gender.Unknown               
+            };
+
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Name = {0}  and Gender = {1}" , customer.Name, GetGender(customer.Gender));
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string GetGender(Gender gender)
+        {
+            switch (gender)
+            {
+                case Gender.Unknown:
+
+                    return "Unknown";
+
+                case Gender.Male:
+                    return "Male";
+
+                case Gender.Female:
+                    return "Female";
+                default:
+                    return "Invalid data detected";
+            }
         }
     }
+}
+
+public enum Gender
+{
+    Unknown,
+    Male,
+    Female
+}
+// 0 - Unknown
+// 1 - Male
+// 2 - Female
+
+public class Customer
+{
+    public string Name { get; set; }
+    public Gender Gender { get; set; }
+
 }
